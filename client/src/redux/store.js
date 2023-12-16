8e72db17a4e74a897297a6245041f09d17e05281
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-12-16 20:11:13 +0600

    only token persisted

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,26 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import { persistStore, persistReducer } from "redux-persist";
+import { persistStore, persistReducer, createTransform } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import authSlice from "./auth/authSlice.js";
 import baseAPI from "./baseAPI.js";
 
-const persistConfig = {
-  key: "root",
+const authSliceTransform = createTransform(
+  (inboundState, key) => {
+    if (key === "authSlice") return { token: inboundState.token };
+    else return inboundState;
+  },
+
+  (outboundState, key) => {
+    if (key === "authSlice") return { token: outboundState.token };
+    else return outboundState;
+  }
+);
+
+const authPersistConfig = {
+  key: "auth",
   storage,
+  transforms: [authSliceTransform],
   whitelist: ["authSlice"],
 };
 
@@ -16,7 +29,7 @@ const rootReducer = combineReducers({
   [baseAPI.reducerPath]: baseAPI.reducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(authPersistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
